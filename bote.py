76360445c2e831842149ab2597429197f8bba75e
Janith Wathsala <janijaniwathsala@gmail.com>
2024-04-11 15:54:36 +0530

    create a bot python file(for experiment)

---
+++ bote.py
@@ -0,0 +1,11 @@
+class ImageToPDFConverter:
+    def __init__(self):
+        pass
+    
+    def convert_image_to_pdf(self, image_path, pdf_path):
+        # Code to convert image to PDF
+        pass
+    
+    def resize_image(self, image_path, width, height):
+        # Code to resize image
+        pass
